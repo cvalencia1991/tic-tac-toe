376c7acf7032fa3f39789c16e55a6293cfcf9417
cvalencia1991 <cesar4a6z@gmail.com>
2024-03-16 14:02:32 -0500

    Change implementation of view
    - Implement Clean Screen each time that the player make a move
    - change view of space of user
    - break when there is a win or a tie

--- ttt.cpp
+++ ttt.cpp
@@ -3,30 +3,30 @@
 #include "ttp_function.hpp"
 
 int main(){
-	int current_player = 1;
-	bool start = true;
+	int current_player = 1, number_of_players;
 	std::string player1, player2;
 	introduction();
 	draw();
-
-	std::cout << "\nEnter name player_1: \n";
+	std::cout << "\nEnter name player_1: ";
 	std::cin >> player1;
-	std::cout << "\nEnter name player_2: \n";
+	std::cout << "Enter name player_2: ";
 	std::cin >> player2;
-	std::cout << "Which player starts? Enter 1 for " << player1 << " or 2 for " << player2 << ": \n";
+	std::cout << "Which player starts? Enter 1 for " << player1 << " or 2 for " << player2 << ": ";
 	std::cin >> current_player;
 	std::string current_player_name = (current_player == 1) ? player1 : player2;
 
-	while(start){
+	while(true){
 		set_position(current_player, current_player_name);
+		clearScreen();
+		introduction();
 		draw();
 		if (is_winner()){
 		std::cout << "Congratulations! " << current_player_name << " wins!\n";
-		start = false;
+		break;
 	}
-	if(filled_up()){
+if(filled_up()){
 		std::cout << "It's a tie!\n";
-		start = false;
+		break;
 	}
 		current_player = (current_player == 1) ? 2 : 1;
 	  current_player_name = (current_player == 1) ? player1 : player2;
