83fb1120323b7908ed680c4de6f453675f10dd39
cvalencia1991 <cesar4a6z@gmail.com>
2024-03-16 10:55:20 -0500

    Change function parameters set_position
    - Implement the way that the look each time that a player select
      a position of the game, this allow us know which person are making the
      move or not

--- ttp_function.hpp
+++ ttp_function.hpp
@@ -1,5 +1,7 @@
+#include <iostream>
+
 void introduction();
 void draw();
-void set_position(int current_player);
+void set_position(int current_player, std::string current_player_name);
 bool is_winner();
 bool filled_up();
